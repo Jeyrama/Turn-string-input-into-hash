59c12eae82f92ede91e0ebd5a32414cfb24e4940
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-09-05 16:45:14 -0700

    Update Turn-string-input-into-hash.js

--- Turn-string-input-into-hash.js
+++ Turn-string-input-into-hash.js
@@ -10,3 +10,16 @@ This string should return a hash that looks like
 
 
 // Solution
+
+function strToHash(str){
+  let result = {}
+  
+  if (str.length) {
+    let items = str.split(", ")
+    items.forEach(item => {
+      const [key, val] = item.split("=")
+      result[key] = +val
+    })  
+  }
+  return result
+}
