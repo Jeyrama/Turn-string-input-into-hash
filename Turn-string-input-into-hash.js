68b36e2e8ef4815f81679b678a749bc54c1c479d
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-09-05 16:44:20 -0700

    Create Turn-string-input-into-hash.js

---
+++ Turn-string-input-into-hash.js
@@ -0,0 +1,9 @@
+/*
+Please write a function that will take a string as input and return a hash. 
+The key will always be a symbol and the value will always be an integer.
+The string will be formatted as such:
+  "a=1, b=2, c=3, d=4"
+
+This string should return a hash that looks like
+{ 'a': 1, 'b': 2, 'c': 3, 'd': 4}
+*/
